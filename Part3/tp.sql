6c00ae86b148c7c14b3b3772f6545f89dcb7c4a2
valros76 <valerian.dufrene@gmail.com>
2021-02-17 11:37:50 +0100

    Add exercices Part 5 and add magic quotes on part3 Changes

--- Part3/tp.sql
+++ Part3/tp.sql
@@ -6,6 +6,6 @@ changer le type de la colonne phoneNumber en VARCHAR
 ajouter les colonnes zipCode(VARCHAR) et city(VARCHAR)*/
 ALTER TABLE `codex.clients`
 DROP `secondPhoneNumber`
-MODIFY phoneNumber VARCHAR(255)
-ADD zipCODE VARCHAR(5),
-city VARCHAR(80)
+MODIFY `phoneNumber` VARCHAR(255)
+ADD `zipCODE` VARCHAR(5),
+`city` VARCHAR(80)
---
+++ Part5/exercice3.sql
@@ -0,0 +1,4 @@
+/*
+Dans la table languages, afficher toutes les versions de PHP et de JavaScript.
+*/
+SELECT `version` FROM `webDevelopment.languages`
