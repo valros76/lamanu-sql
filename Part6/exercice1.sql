9c1e644e6d52762d256a5fcf7b3a1f84d1164ba4
valros76 <valerian.dufrene@gmail.com>
2021-02-17 13:54:18 +0100

    Part 6 : Done

---
+++ Part6/exercice1.sql
@@ -0,0 +1,6 @@
+/*
+Dans la table frameworks, afficher toutes les données de la table ayant une version 2.x (x étant un numéro quelconque).
+EDIT : cibler la table 'ide' et la version '3.x'
+*/
+SELECT `version` FROM `webDevelopment.ide`
+WHERE `version` LIKE '%3.%';
