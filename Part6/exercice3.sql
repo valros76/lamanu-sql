a6ada73c0e4152c8882617ccb4c45b0da56d536c
valros76 <valerian.dufrene@gmail.com>
2021-02-17 13:54:18 +0100

    Part 6 : Done

---
+++ Part6/exercice3.sql
@@ -0,0 +1,4 @@
+/*
+Dans la table ide, afficher toutes les lignes ayant une date comprise entre le premier janvier 2010 et le 31 decembre 2011.
+*/
+SELECT * FROM `webDevelopment.ide` WHERE `date` BETWEEN '2010-01-01' AND '2011-12-31';
