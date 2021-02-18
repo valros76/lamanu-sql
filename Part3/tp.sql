97b6f3d33eb49220aa25cd85f42097bac72a33b5
valros76 <valerian.dufrene@gmail.com>
2021-02-18 09:48:53 +0100

    Modify tp on part 3, one line forget

--- Part3/tp.sql
+++ Part3/tp.sql
@@ -6,6 +6,7 @@ changer le type de la colonne phoneNumber en VARCHAR
 ajouter les colonnes zipCode(VARCHAR) et city(VARCHAR)*/
 ALTER TABLE `codex`.`clients`
 DROP `secondPhoneNumber`
+CHANGE `firstPhoneNumber` `phoneNumber` INT
 MODIFY `phoneNumber` VARCHAR(255)
 ADD `zipCODE` VARCHAR(5),
 `city` VARCHAR(80);
