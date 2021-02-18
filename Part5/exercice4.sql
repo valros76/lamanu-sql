9b8181fbf14e005148ca6d97a94d5e2f2b4c7111
valros76 <valerian.dufrene@gmail.com>
2021-02-18 10:01:32 +0100

    Affine magic quotes and add commentary for IN notation

--- Part5/exercice4.sql
+++ Part5/exercice4.sql
@@ -1,4 +1,9 @@
 /*
 Dans la table languages, afficher toutes les lignes ayant pour id 3,5,7.
 */
-SELECT * FROM `webDevelopement.languages` WHERE `id` = 3 OR `id` = 5 OR `id` = 7;
+SELECT * FROM `webDevelopement`.`languages` WHERE `id` = 3 OR `id` = 5 OR `id` = 7;
+/*
+Notation avec IN
+SELECT * FROM `webDevelopement`.`languages`
+WHERE `id` IN(3,5,7)
+*/
--- Part5/exercice5.sql
+++ Part5/exercice5.sql
@@ -1,4 +1,4 @@
 /*
 Dans la table languages, afficher les deux première entrées de JavaScript.
 */
-SELECT version FROM `languages` WHERE version LIKE '%JAVASCRIPT%' ORDER BY `id` LIMIT 0,2;
+SELECT `version` FROM `languages` WHERE version LIKE '%JAVASCRIPT%' ORDER BY `id` LIMIT 0,2;
